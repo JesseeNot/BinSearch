2935c304d40a9c72a42aaa7493e6bcdff4e3e425
Jessee <JesseeNot@zhangyunhaodemacbook-pro.local>
2016-02-06 10:37:47 +0800

    Initial Commit

---
+++ BinSearch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinSearch
+//
+//  Created by 张云皓 on 16/2/6.
+//  Copyright © 2016年 Jessee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
